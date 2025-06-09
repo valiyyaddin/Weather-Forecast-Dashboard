ad4ef43377f07cc7efde62ed6f4fd5f476bf7404
valiyyaddin <eliyevveliyeddin3@gmail.com>
2025-06-09 20:49:52 +0400

    Add interface for the app

---
+++ main.py
@@ -0,0 +1,9 @@
+import streamlit as st
+st.title("Weather Forecast for Next Days")
+
+place=st.text_input("Place:")
+days=st.slider("Forecast Days",min_value=1,max_value=5,
+               help="Select number of Forecasted days")
+option=st.selectbox("Select data  to view",
+                    ("Temperature","Sky"))
+st.subheader(f"{option} for the next 2 days in {place}")
