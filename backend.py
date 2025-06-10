de1b26d00f6c54781dfc53edaa108f80135135c9
valiyyaddin <eliyevveliyeddin3@gmail.com>
2025-06-10 16:24:07 +0400

    Add backend test for places and temperatures

---
+++ backend.py
@@ -0,0 +1,20 @@
+import requests
+
+API_KEY="b50c71c35370985a05337630b5ce83c7"
+
+def get_data(place,forecast_days,kind = None):
+    url=f"http://api.openweathermap.org/data/2.5/forecast?q={place}&appid={API_KEY}"
+    response = requests.get(url)
+    data = response.json()
+    filtered_data=data['list']
+    nr_values = 8*forecast_days
+    filtered_data = filtered_data[:nr_values]
+    if kind =="Temperature":
+        filtered_data = [dict["main"]["temp"] for dict in filtered_data]
+        return  filtered_data
+    if kind =="Sky":
+        filtered_data = [dict["weather"][0]["main"] for dict in filtered_data]
+    return filtered_data
+
+if __name__ == "__main__":
+    print(get_data(place="Tokyo",forecast_days=3, kind="Temperature"))
