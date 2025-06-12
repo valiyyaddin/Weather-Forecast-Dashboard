51e45efa11aea4e5c635efa28c1c2b90c98255f3
valiyyaddin <eliyevveliyeddin3@gmail.com>
2025-06-12 12:09:48 +0400

    Add sky conditions, filter data, then show pictures for conditions

--- main.py
+++ main.py
@@ -9,14 +9,15 @@ days=st.slider("Forecast Days",min_value=1,max_value=5,
 option=st.selectbox("Select data  to view",
                     ("Temperature","Sky"))
 st.subheader(f"{option} for the next {days} days in {place}")
-
-data=get_data(place,days,option)
-
-
-
-
-
-figure=px.line(x = d, y = t, labels={"x": "Date", "y": "Temperature (C)"})
-
-
-st.plotly_chart(figure)
+if place:
+    filtered_data = get_data(place,days)
+    if option == "Temperature":
+        temperatures = [dict["main"]["temp"] for dict in filtered_data]
+        dates = [dict["dt_txt"] for dict in filtered_data]
+        figure=px.line(x = dates, y = temperatures, labels={"x": "Date", "y": "Temperature (C)"})
+        st.plotly_chart(figure)
+    if option == "Sky":
+        images = {"Clear" : "images/clear.png", "Clouds" : "images/cloud.png","Rain" : "images/rain.png", "Snow" : "images/snow.png"}
+        sky_conditions = [dict["weather"][0]["main"] for dict in filtered_data]
+        images_paths = [images[condition] for condition in sky_conditions]
+        st.image(images_paths)
